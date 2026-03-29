69ef4fe42b562c7cdc82ae8137f4e3d177510ef5
Josuah Demangeon <me@josuah.net>
2026-03-29 20:21:57 +0200

    zephyr: remove: migrated to priv-tvai-usb

--- zephyr/boards/common/ecpprog_mpremote.board.cmake
+++
@@ -1,6 +0,0 @@
-# SPDX-License-Identifier: Apache-2.0
-
-board_set_flasher_ifnset(ecpprog_mpremote)
-
-board_finalize_runner_args(ecpprog_mpremote
-  "--mpremote-script=${ZEPHYR_TINYCLUNX33_MODULE_DIR}/zephyr/scripts/mpremote/full_reset.py")
