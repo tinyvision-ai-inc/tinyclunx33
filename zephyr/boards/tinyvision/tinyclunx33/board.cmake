3225a22c864cfefe05479abe3fc40b3d68c4c0ad
Josuah Demangeon <me@josuah.net>
2026-03-29 17:23:36 +0200

    zephyr: cmake: move USB driver relocation to board def

--- zephyr/boards/tinyvision/tinyclunx33/board.cmake
+++ zephyr/boards/tinyvision/tinyclunx33/board.cmake
@@ -9,3 +9,7 @@ include(${CMAKE_CURRENT_LIST_DIR}/../../common/ecpprog_mpremote.board.cmake)
 board_runner_args(openocd --cmd-pre-init "source [find tinyclunx33.cfg]")
 
 include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
+
+# Run some code from RAM for performance during enumeration
+zephyr_code_relocate(LIBRARY drivers__usb__udc LOCATION RAM)
+#zephyr_code_relocate(LIBRARY subsys__usb__device_next LOCATION RAM) # Not enough RAM
